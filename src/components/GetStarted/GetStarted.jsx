49b01b8b89e4593ae53b847131d03966bc6ecaad
Muhammad Asif <alexasifhhossain@gmail.com>
2023-05-30 16:20:30 +0600

    Newsletter section completed

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './GetStarted.css'
+
+function GetStarted() {
+  return (
+    <div className="g-wrapper">
+        <div className="paddings innerWidth g-container">
+            <div className="flexColCenter inner-container">
+                <span className='primaryText'>Get Started with Homyz</span>
+                <span className='secondaryText'>Subscribe and find super attractive price quotes from our Newsletter
+                <br />Find your residence soon
+                </span>
+                
+                <button className='button'>
+                    <a href="mailto:asifhossainhridoy10@gmail.com">Get Started</a>
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+export default GetStarted;
